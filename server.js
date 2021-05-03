48e699b47125523592f15390f30de11dfab99ef0
Michael Metcalf <michaelmetcalf52@gmail.com>
2021-05-03 21:17:05 +0900

    began installing dependencies for user authentication

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const port = 8080;
 
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log("Listening on port ", port);
+var corsOptions = {
+  origin: "http://localhost.8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-typ - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  console.log("Welcome to GamaGacha!");
+  res.send("Welcome to GamaGacha!");
+});
+
+// set port and listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
 });
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 8081;
+const port = 8082;
 
 app.use(express.json());
 
