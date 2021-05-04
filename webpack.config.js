2b04a294c50060782866ae70118acca39f70a17e
Michael Metcalf <michaelmetcalf52@gmail.com>
2021-05-04 23:59:16 +0900

    configured webpack to bundle files in a dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.join(__dirname, "public")
+    path: path.join(__dirname, "dist")
   },
 
   module: {
